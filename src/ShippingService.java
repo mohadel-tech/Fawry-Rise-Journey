89a0ab1457ff0009a01e0eaa955c3fcef88bbdce
Mohamed Adel <Mohammed-Dol3.com>
2025-07-05 05:48:21 +0300

    Update shipment method to group quantities

--- src/ShippingService.java
+++ src/ShippingService.java
@@ -1,5 +1,6 @@
-
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ShippingService {
     private static final double SHIPPING_RATE_PER_KG = 30.0;
@@ -8,14 +9,28 @@ public class ShippingService {
         if (items.isEmpty()) return;
 
         System.out.println("** Shipment notice **");
-        double totalWeight = 0;
+        
+        Map<String, Integer> itemCounts = new HashMap<>();
+        Map<String, Double> itemWeights = new HashMap<>();
         
         for (Shippable item : items) {
-            System.out.println("1x " + item.getName() + " " + (item.getWeight() * 1000) + "g");
-            totalWeight += item.getWeight();
+            String name = item.getName();
+            itemCounts.put(name, itemCounts.getOrDefault(name, 0) + 1);
+            itemWeights.put(name, item.getWeight());
         }
         
-        System.out.println("Total package weight " + totalWeight + "kg ");
+        double totalWeight = 0;
+        
+        for (Map.Entry<String, Integer> entry : itemCounts.entrySet()) {
+            String name = entry.getKey();
+            int quantity = entry.getValue();
+            double weight = itemWeights.get(name);
+            
+            System.out.println(quantity + "x " + name + " " + (weight * 1000) + "g");
+            totalWeight += weight * quantity;
+        }
+        
+        System.out.println("Total package weight " + String.format("%.2f", totalWeight)+ "kg");
     }
 
     public static double calculateShippingFee(List<Shippable> items) {
